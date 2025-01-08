5142c89aec2d7b2fba4ce2459161f3ae13a2ea45
wonsky <pidborskii17@gmail.com>
2025-01-09 00:29:02 +0100

    add docstring

--- tools/utils.py
+++ tools/utils.py
@@ -1,10 +1,18 @@
-from typing import List
+from typing import List, Optional
 from github.Repository import Repository
 
 
 def get_all_repository_paths(repo: Repository, path: str = "") -> List[str]:
+    """
+    Get all file paths in a GitHub repository.
+
+    :param repo: The GitHub repository object.
+    :param path: The path within the repository to start from (default is root).
+    :return: A list of all file paths in the repository.
+    """
     paths = []
     contents = repo.get_contents(path)
+
     for content in contents:
         if content.type == "dir":
             paths.extend(get_all_repository_paths(repo, content.path))
