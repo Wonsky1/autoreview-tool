52ff275add7bda11d00802e48b237304f590fd3c
wonsky <pidborskii17@gmail.com>
2025-01-09 15:05:43 +0100

    config update for github validation

--- core/config.py
+++ core/config.py
@@ -1,9 +1,11 @@
 from logging import getLogger
+
+from github import Github, Auth
 from pydantic_settings import BaseSettings
 from pydantic import field_validator, ValidationInfo
 from langchain_community.chat_models import ChatOpenAI
 from langchain_ollama import ChatOllama
-from typing import Optional, Union
+from typing import Optional
 
 logger = getLogger(__name__)
 
@@ -17,13 +19,12 @@ class Settings(BaseSettings):
     OPENAI_API_KEY: Optional[str] = None
     GENERATIVE_MODEL: Optional[ChatOllama | ChatOpenAI] = None
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-        case_sensitive = True
+    github_client: Optional[Github] = None
 
     @field_validator("GENERATIVE_MODEL", mode="before")
-    def validate_generative_model(cls, value: Optional[ChatOpenAI | ChatOllama], info: ValidationInfo):
+    def validate_generative_model(
+        cls, value: Optional[ChatOpenAI | ChatOllama], info: ValidationInfo
+    ):
         """
         Validates and initializes the appropriate generative model based on LOCAL_DEVELOPMENT.
 
@@ -43,10 +44,43 @@ class Settings(BaseSettings):
 
         openai_api_key = info.data.get("OPENAI_API_KEY")
         if not openai_api_key:
-            raise ValueError("OPENAI_API_KEY must be provided when LOCAL_DEVELOPMENT is False.")
+            raise ValueError(
+                "OPENAI_API_KEY must be provided when LOCAL_DEVELOPMENT is False."
+            )
 
         logger.info("Using remote model: OpenAI GPT")
-        return ChatOpenAI(model=info.data.get("OPENAI_MODEL_NAME", "gpt-4-turbo"), openai_api_key=openai_api_key)
+        return ChatOpenAI(
+            model=info.data.get("OPENAI_MODEL_NAME", "gpt-4-turbo"),
+            openai_api_key=openai_api_key,
+        )
+
+    @field_validator("github_client", mode="before")
+    def validate_github_client(cls, value: Optional[Github], info: ValidationInfo):
+        """
+        Validates and initializes the GitHub client.
+
+        Args:
+            value: The current value of the github_client field.
+            info: Validation information containing other field values.
+
+        Returns:
+            The initialized GitHub client.
+
+        Raises:
+            ValueError: If GITHUB_ACCESS_TOKEN is missing.
+        """
+        github_access_token = info.data.get("GITHUB_ACCESS_TOKEN")
+        if not github_access_token:
+            raise ValueError("GITHUB_ACCESS_TOKEN must be provided.")
+
+        logger.info("Initializing GitHub client...")
+        auth = Auth.Token(github_access_token)
+        return Github(auth=auth)
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
 
 
 settings = Settings()
